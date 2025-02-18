16fdd97446825ecae5f3252ae4e31bbe0d762c5b
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2025-02-18 16:38:34 +0530

    changed state-backend path

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {  //Day 3 - state backend with s3 and dynamodb
     bucket = "ipd-s3-state-bucket"
-    key    = "terraform-state-jenkins"
+    key    = "terraform-state-jenkins-demo"
     region = "us-east-1"
     dynamodb_table = "ipd-dynamo-state-backend"
     encrypt = true
