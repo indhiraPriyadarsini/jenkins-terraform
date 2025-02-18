fb9ec709b5fb9ef9b10f28d2c616163b1a02e93d
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2025-02-18 16:59:02 +0530

    reconfg

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
-  backend "s3" {  //Day 3 - state backend with s3 and dynamodb
-    bucket = "ipd-s3-state-bucket"
-    key    = "terraform-state-jenkins-demo"
-    region = "us-east-1"
+  backend "s3" { //Day 3 - state backend with s3 and dynamodb
+    bucket         = "ipd-s3-state-bucket"
+    key            = "terraform-state-jenkins-demo"
+    region         = "us-east-1"
     dynamodb_table = "ipd-dynamo-state-backend"
-    encrypt = true
+    encrypt        = true
   }
 }
 
 provider "aws" {
-    region = var.aws_region
-    profile = "default"
+  region = var.aws_region
+  # profile = "default"
 }
